0bfa52d2b7c3ee08fce46e422e697390d3d1c24c
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2026-01-23 12:26:52 +0100

    test open url

--- lib/widgets/le_monde_items.dart
+++ lib/widgets/le_monde_items.dart
@@ -1,13 +1,11 @@
 import 'package:efrei_flutter_2026/models/une_le_monde.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LeMondeItem extends StatelessWidget {
   final UneLeMonde item;
 
-  const LeMondeItem({
-    super.key,
-    required this.item,
-  });
+  const LeMondeItem({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +14,14 @@ class LeMondeItem extends StatelessWidget {
       subtitle: Text(item.description),
       onTap: () {
         // You can add navigation to detail page here if needed
+        _launchUrl(item.link);
       },
     );
+  }
+
+  Future<void> _launchUrl(String url) async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
