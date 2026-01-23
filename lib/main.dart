3bc663e90a835860831182e9d52bde0e6dd4f77c
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2026-01-23 12:02:20 +0100

    rename home page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Le Monde RSS'),
+      home: const MyHomePage(title: 'Client RSS'),
     );
   }
 }
